c47125b1be7cabaaba338ea6cb91c1ff1823b4d7
Fernando Muñoz <fernandrewm@gmail.com>
2021-01-07 12:50:21 -0600

    Código básico del juego terminado, faltan mejoras

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -8,16 +8,20 @@
  let deck = [];
  const tipos = ['C', 'D', 'H', 'S'];
  const especiales = ['A', 'J', 'Q', 'K'];
+
  let puntosJugador = 0,
      puntosComputadora = 0;
 
  //Referencias del HTML
  const btnPedir = document.querySelector('#btnPedir');
  const btnDetener = document.querySelector('#btnDetener');
- const puntosHTML = document.querySelectorAll('small');
+ const btnNuevo = document.querySelector('#btnNuevo');
+
  const divCartasJugador = document.querySelector('#jugador-cartas');
  const divCartasComputadora = document.querySelector('#computadora-cartas');
 
+ const puntosHTML = document.querySelectorAll('small');
+ 
  // Esta función crea un nuevo deck
  const crearDeck = () => {
     for( let i = 2; i <= 10; i++){
@@ -41,19 +45,18 @@
 
  //Esta función me permite tomar una carta
  const pedirCarta = () => {
+
      if ( deck.length === 0){
          throw 'No hay cartas en el deck';
     }
-
     const carta = deck.pop();
-
     return carta;
  }
 
  //Función para asignar valor a carta
 const valorCarta = ( carta ) => {
+
     const valor = carta.substring(0, carta.length - 1);
-    
     return ( isNaN(valor) ) ? 
         ( valor === 'A' ) ? 11 : 10
         : valor * 1;
@@ -61,8 +64,10 @@ const valorCarta = ( carta ) => {
 
 //Turno computadora
 const turnoComputadora = (puntosMinimos) => {
+    
     do {
         const carta = pedirCarta();
+
         puntosComputadora = puntosComputadora + valorCarta(carta);
         puntosHTML[1].innerText = puntosComputadora;
     
@@ -74,7 +79,20 @@ const turnoComputadora = (puntosMinimos) => {
         if (puntosMinimos > 21){
             break;
         }
+
     } while((puntosComputadora < puntosMinimos) && (puntosMinimos <= 21));
+
+    setTimeout( () => {
+        if(puntosComputadora === puntosMinimos){
+            alert('Nadie gana :(');
+        } else if (puntosMinimos > 21) {
+            alert('La computadora gana');
+        } else if (puntosComputadora > 21){
+            alert('Jugador gana');
+        } else {
+            alert('Computadora gana');
+        }
+    }, 100);
 }
 
 // Eventos
@@ -106,3 +124,23 @@ btnDetener.addEventListener('click', () => {
     turnoComputadora(puntosJugador);
     btnDetener.disabled = true;
 })
+
+btnNuevo.addEventListener('click', () => {
+    console.clear();
+    
+    deck = [];
+    deck = crearDeck();
+
+    puntosJugador = 0;
+    puntosComputadora = 0;
+
+    puntosHTML[0].innerText = 0;
+    puntosHTML[1].innerText = 0;
+
+    divCartasComputadora.innerHTML = '';
+    divCartasJugador.innerHTML = '';
+
+    btnPedir.disabled = false;
+    btnDetener.disabled = false;
+
+});

576b59afea0b3318794d98e963f10e50c8c17ad1
Fernando Muñoz <fernandrewm@gmail.com>
2021-01-06 18:42:18 -0600

    Funcion pedir carta creada

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -9,6 +9,7 @@
  const tipos = ['C', 'D', 'H', 'S'];
  const especiales = ['A', 'J', 'Q', 'K'];
 
+ // Esta función crea un nuevo deck
  const crearDeck = () => {
     for( let i = 2; i <= 10; i++){
         for( let tipo of tipos){
@@ -21,11 +22,25 @@
             deck.push(esp + tipo)
         }
     }
-    console.log(deck);
+
     deck = _.shuffle(deck);
     console.log(deck);
-    
+
     return deck
  }
 
  crearDeck();
+
+ //Esta función me permite tomar una carta
+ const pedirCarta = () => {
+     if ( deck.length === 0){
+         throw 'No hay cartas en el deck';
+    }
+
+    const carta = deck.pop();
+    
+    console.log(carta);
+    console.log(deck);
+ }
+
+//  pedirCarta();
